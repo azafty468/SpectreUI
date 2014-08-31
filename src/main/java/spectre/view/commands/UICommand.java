cca3aaf425f28e7046c1d20b96008fa3c6446d63
azafty468 <azafty@gmail.com>
2014-08-31 16:28:16 -0400

    resyncing with new project structure

--- src/main/java/spectre/view/commands/UICommand.java
+++
@@ -1,9 +0,0 @@
-package spectre.view.commands;
-
-/**
- * Created by andrewzafft on 6/3/14.
- *
- * The interpretation of the clicks and key pressed made by the page controller that is
- */
-public class UICommand {
-}
